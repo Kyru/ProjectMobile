3d40b798e8ab47776ce71b85f9be59a0428ab6ae
Alexander Brazda <alexbrazda@ofiben.com>
2019-07-08 17:27:09 +0200

    KeyBoardTest

---
+++ MobilePrototype/Assets/Scripts/KeyboardTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardTest : MonoBehaviour
+{
+    [SerializeField] Vector3[] positions;
+    int actualPosition;
+    int maxPosition;
+    int minPosition;
+    Vector3 originalScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minPosition = 0;
+        maxPosition = positions.Length;
+        actualPosition = 1;         // posición mitad
+        transform.position = positions[actualPosition];
+
+        originalScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("down"))
+        {
+            print("I pressed down");
+            if (actualPosition - 1 >= minPosition)
+            {
+                actualPosition--;
+                transform.position = positions[actualPosition];
+            }
+        }
+
+        if (Input.GetKeyDown("up"))
+        {
+            print("I pressed up");
+            if (actualPosition + 1 <= maxPosition)
+            {
+                actualPosition++;
+                transform.position = positions[actualPosition];
+            }
+        }
+    }
+}
