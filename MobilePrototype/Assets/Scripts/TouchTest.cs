8e8e3a4ce20b3c763e8701335c251d6cfa783be7
Fer <ferraniusd@gmail.com>
2019-07-08 16:14:07 +0200

    Primer push

---
+++ MobilePrototype/Assets/Scripts/TouchTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour
+{
+    [SerializeField] Vector3[] positions;
+    int actualPosition;
+    int maxPosition;
+    int minPosition;
+    Vector3 originalScale;
+    int stationaryFingerId;
+
+    void Start()
+    {
+        minPosition = 0;
+        maxPosition = positions.Length;
+        actualPosition = 1;         // posición mitad
+        transform.position = positions[actualPosition];
+
+        originalScale = transform.localScale;
+    }
+    void Update()
+    {
+        Touch[] myTouches = Input.touches;
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (myTouches[i].phase == TouchPhase.Began)
+            {
+                if (myTouches[i].position.x < (Screen.width / 2) && myTouches[i].position.y < (Screen.height / 2))  // abajo izquierda
+                {
+                    if (actualPosition - 1 >= minPosition) actualPosition--;
+                    transform.position = positions[actualPosition];
+                }
+                else if (myTouches[i].position.x < (Screen.width / 2) && myTouches[i].position.y > (Screen.height / 2))  // arriba izquierda
+                {
+                    if (actualPosition + 1 <= maxPosition) actualPosition++;
+                    transform.position = positions[actualPosition];
+                }
+            }
+            else if (myTouches[i].phase == TouchPhase.Stationary)
+            {
+                if (myTouches[i].position.x >= (Screen.width / 2))         // parte derecha
+                {
+                    stationaryFingerId = myTouches[i].fingerId;
+                    transform.localScale += new Vector3(0, 0, 0.1f);
+                }
+            }
+            else if (myTouches[i].phase == TouchPhase.Moved && myTouches[i].fingerId == stationaryFingerId)
+            {
+                if (myTouches[i].position.x >= (Screen.width / 2))         // parte derecha
+                {
+                    transform.localScale += new Vector3(0, 0, 0.1f);
+                }
+            }
+            else if (myTouches[i].phase == TouchPhase.Ended && myTouches[i].fingerId == stationaryFingerId)
+            {
+                transform.localScale = originalScale;
+            }
+        }
+    }
+}
