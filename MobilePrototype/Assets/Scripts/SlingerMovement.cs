ecf685bdf0f281b75f2974b91cfe8aaf8b0eeb83
Fer <ferraniusd@gmail.com>
2019-07-10 17:34:50 +0200

    Escena The Hole creada

---
+++ MobilePrototype/Assets/Scripts/SlingerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingerMovement : MonoBehaviour
+{
+    public float thrust = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("playing");
+            GetComponent<Rigidbody>().AddForce(transform.forward * thrust, ForceMode.Impulse);
+        }
+    }
+}
